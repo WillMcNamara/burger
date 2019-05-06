71a982c5ed2faaae1ec5c65a921cbfb48d356f01
Will M <wmcna97@gmail.com>
2019-05-06 15:24:38 -0400

    some route work in controller, filled server.js, some handlebars html code

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var burger = require("../models/burger");
+
+var router = express.Router();
+
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        var obj = {
+            burgers: data
+        };
+    res.render("index", obj);
+    })
+})
+
+router.post("/new/:burger", function(req, res){
+    res.render("index");
+})
+
+router.update("/update/:burger", function(req, res){
+    res.render("index");
+})
+
+module.exports = router;
